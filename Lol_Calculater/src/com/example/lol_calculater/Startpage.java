6db303c891e5ad2cf42f2eebe9c4f6c15da4784e
mikkusu <the_mizukage@yahoo.com>
2014-09-09 15:48:51 -0400

    Test
    This is a test to Commit a black project

---
+++ Lol_Calculater/src/com/example/lol_calculater/Startpage.java
@@ -0,0 +1,15 @@
+package com.example.lol_calculater;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Startpage extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_startpage);
+	}
+}
