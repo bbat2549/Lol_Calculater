bb77009faeb6ed27b8172bd1666ec22a7508dd4d
mikkusu <the_mizukage@yahoo.com>
2014-09-18 15:37:12 -0400

    Buttons
    This has the layout of the champion select screen

---
+++ LoL_Cal/src/com/example/lol_cal/Startscreen.java
@@ -0,0 +1,13 @@
+package com.example.lol_cal;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Startscreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_startscreen);
+	}
+}
