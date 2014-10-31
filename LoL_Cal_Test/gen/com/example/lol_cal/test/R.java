e90d598d33cd58ba95a5a1172931228305fc285e
mikkusu <the_mizukage@yahoo.com>
2014-10-31 16:00:32 -0400

    ...

--- LoL_Cal_Test/gen/com/example/lol_cal/test/R.java
+++ LoL_Cal_Test/gen/com/example/lol_cal/test/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package course.examples.helloWorldWithLogin.test;
+package com.example.lol_cal.test;
 
 public final class R {
     public static final class attr {
